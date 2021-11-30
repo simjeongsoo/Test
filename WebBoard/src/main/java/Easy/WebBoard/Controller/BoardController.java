633c6451bfc6e17b1f80d087aaa8e2b5bbba797c
simjeongsoo <simking95@naver.com>
2021-11-30 15:07:07 +0900

    게시글 리스트,상세 추가(controller)

--- WebBoard/src/main/java/Easy/WebBoard/Controller/BoardController.java
+++ WebBoard/src/main/java/Easy/WebBoard/Controller/BoardController.java
@@ -4,12 +4,16 @@ package Easy.WebBoard.Controller;
 import Easy.WebBoard.entity.Board;
 import Easy.WebBoard.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.SQLOutput;
+
+//===========================================================================================
 
 @Controller
 public class BoardController {
@@ -23,16 +27,21 @@ public class BoardController {
 //        return "Hello";
 //    }
 
+//===========================================================================================
     @Autowired
     private BoardService boardService;
+//===========================================================================================
 
-
+    // 게시글 작성 페이지
     @GetMapping("/board/write") // locallhost:8080/board/write
     public String boardWriteForm(){
 
         return"boardwrite";
     }
 
+//===========================================================================================
+
+    //db 삽입 페이지
     //post url과 boardwrite.htlm의 form 태그의 action url이 일치해야 함
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board){
@@ -44,8 +53,30 @@ public class BoardController {
         return"";
     }
 
+//===========================================================================================
+
+    // 게시글 리스트 페이지
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+
+        //데이터를 담아 페이지로 던져줄 모델
+        model.addAttribute("list",boardService.boardList());
+
+        return "boardlist";
+    }
+
+//===========================================================================================
+
+    //게시글 상세 페이지
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardview";
+    }
 
 
 
-
-}
+//===========================================================================================
+}//end
